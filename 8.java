04350a63c11d4c7fd4c72fb7417fab62f465b47a
S7R0K3R <46674147+kingstroke@users.noreply.github.com>
2019-11-13 00:04:51 +0530

    Create 8.java

---
+++ 8.java
@@ -0,0 +1,17 @@
+package kingstroke;
+import java.util.Scanner;
+
+class apples {
+	public static void main(String args[]) {
+		Scanner obj = new Scanner (System.in);
+		int girls, boys, people;
+		//double girls, boys, people;
+		girls = 11;
+		boys = 3;
+		//people = girls + boys;
+		//people = girls * boys;
+		//people = girls / boys;
+		people = girls % boys;
+		System.out.println(people);
+	}
+}
