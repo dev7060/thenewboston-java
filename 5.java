bc4aac159699e10b41c4c2fc3865ac6bab18167a
S7R0K3R <46674147+kingstroke@users.noreply.github.com>
2019-11-12 23:44:27 +0530

    Create 5.java

---
+++ 5.java
@@ -0,0 +1,12 @@
+package kingstroke;
+
+class apples {
+	public static void main(String args[]) {
+		double variable;
+		variable = 5.28;
+		System.out.print("Value is : ");
+		System.out.print(variable);
+		System.out.println(" Dollars");
+		System.out.print("apples");
+	}
+}
