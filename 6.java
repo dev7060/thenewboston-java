3e1af2ff27946650f0d40f826dec6e099baa4070
S7R0K3R <46674147+kingstroke@users.noreply.github.com>
2019-11-12 23:44:34 +0530

    Create 6.java

---
+++ 6.java
@@ -0,0 +1,9 @@
+package kingstroke;
+import java.util.Scanner;
+
+class apples {
+	public static void main(String args[]) {
+		Scanner kingStroke = new Scanner(System.in);
+		System.out.println(kingStroke.nextLine());
+	}
+}
