0afcd0e8a54ddd025bb83014ed4bc2f2f22f5e73
S7R0K3R <46674147+kingstroke@users.noreply.github.com>
2019-11-14 21:45:02 +0530

    Create 10.java

---
+++ 10.java
@@ -0,0 +1,12 @@
+package kingstroke;
+
+class apples {
+	public static void main(String args[]) {
+		int test = 6;
+		if (test == 9) {
+			System.out.println("yes");
+		}else {
+			System.out.println("this is else!");
+		}
+	}
+}
