022691615f7c23cd2f2aa9f465ff829ff09fea57
S7R0K3R <46674147+kingstroke@users.noreply.github.com>
2019-11-15 22:42:42 +0530

    18

---
+++ 18/1.java
@@ -0,0 +1,17 @@
+package kingstroke;
+
+class apples {
+	public static void main(String args[]) {
+		int age = 60;
+		if(age<50) {
+			System.out.println("you are young");
+		}else {
+			System.out.println("you are old");
+			if(age>75) {
+				System.out.println("you are REALLY old");
+			}else {
+				System.out.println("don't worry you aren't really that old");
+			}
+		}
+	}
+}
