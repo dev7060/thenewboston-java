6c550f12aaa7fc43b1d3dec610d365d913255cf1
S7R0K3R <46674147+kingstroke@users.noreply.github.com>
2019-11-15 22:42:48 +0530

    19

---
+++ 19/1.java
@@ -0,0 +1,15 @@
+package kingstroke;
+
+class apples {
+	public static void main(String args[]) {
+			int age = 45;
+			if(age>=60)
+				System.out.println("senior");
+			else if(age>=50)
+				System.out.println("in 50s");
+			else if(age>=40)
+				System.out.println("in 40s");
+			else
+				System.out.println("young");
+	}
+}
